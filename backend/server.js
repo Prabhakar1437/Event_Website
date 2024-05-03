779f7c5a05547417e92f3c40ece9212ad735e9e0
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-03 17:57:33 +0530

    Version 3.0

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 import app from "./app.js";
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const frontendBuildPath = path.join(process.cwd(), "frontend", "build");
 
 if(process.env.NODE_ENV === "production"){
-  const frontendBuildPath = path.resolve(__dirname, "../frontend/build");
   app.use(express.static(frontendBuildPath));
   app.get("*", (req, res) => {
     res.sendFile(path.join(frontendBuildPath, "index.html"));
