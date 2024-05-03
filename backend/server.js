abed3b4cac79906a3fd65e3b8c8e1c01e60eda05
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-03 17:07:55 +0530

    FirstCommit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import app from "./app.js";
+
+const path= require("path");
+__dirname = path.resolve();
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
+});
---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/message/send",
+        {
+          name,
+          email,
+          subject,
+          message,
+        },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        toast.success(res.data.message);
+        setName("");
+        setEmail("");
+        setMessage("");
+        setSubject("");
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+
+  return (
+    <>
+      <div className="contact container">
+        <div className="banner">
+          <div className="item">
+            <h4>Address</h4>
+            <p>Begusarai,Bihar ,851101</p>
+          </div>
+          <div className="item">
+            <h4>Call Us</h4>
+            <p>Call Us: +918651960895</p>
+          </div>
+          <div className="item">
+            <h4>Mail Us</h4>
+            <p>vats@gmail.com</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="item">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13719.388935099598!2d76.69900595!3d30.722694799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1714734925442!5m2!1sen!2sin"
+              style={{ border: 0, width: "100%", height: "450px" }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
+          <div className="item">
+            <form onSubmit={handleSendMessage}>
+              <h2>CONTACT</h2>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                  type="email"
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="Subject"
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+              <textarea
+                rows={10}
+                placeholder="Message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
