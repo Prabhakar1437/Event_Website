520eb76431e33b3a25f96ba3eab02591f88c162c
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-03 17:43:26 +0530

    Deploy error fix

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import app from "./app.js";
 import express from "express";
-import {dirname} from "path";
+import path from "path";
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
