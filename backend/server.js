6d34302eb09274186b2b769fcc36d57197d26c7f
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-03 17:50:29 +0530

    new feature

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
 
