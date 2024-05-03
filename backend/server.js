be2089a05ee9de45bc4127eb0d511da715b77f9a
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-05-03 17:46:29 +0530

    Error fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "/frontend/build")));
